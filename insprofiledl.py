11e659e1f53cb837544c1378456ffe36d131c595
Jolie Grace <41444935+joliegrace@users.noreply.github.com>
2021-09-17 00:07:34 +0700

    update insprofiledl.py

--- insprofiledl.py
+++ insprofiledl.py
@@ -1,4 +1,3 @@
-
 import requests
 import time
 import calendar
@@ -52,8 +51,8 @@ def login():
                     "x-csrftoken":csrftoken,
                     "cookie":cookiesStr}
 	form_payload = {'username':username,
-                    'enc_password':'#PWD_INSTAGRAM_BROWSER:0:'+timestamp+':'+password}
-	auth_resp = requests.post(BASE_URL+LOGIN_ENDPOINT,headers=auth_headers,data=form_payload)
+                    'enc_password':f"#PWD_INSTAGRAM_BROWSER:0:{timestamp}:{password}"}
+	auth_resp = requests.post(BASE_URL + LOGIN_ENDPOINT , headers = auth_headers , data = form_payload)
 	AuthCheck(auth_resp)
 	
 def AuthCheck(response):
@@ -85,11 +84,14 @@ def prepareScap():
 	
 	
 def Scap(ssid):
-	profile_name = input('>>Enter profile username : ') + '/'
+	profile_name = input('>>Enter profile username : ') 
+	if profile_name == '':
+		print('------ Empty ! enter username again ')
+		Scap(ssid)
 	profile_headers = {'user-agent':USER_AGENT,
-								  	'cookie':'sessionid=' + ssid}
+					   'cookie':'sessionid=' + ssid}
 	print('------------- Sending request.... -------------')
-	profile_resp = requests.get(BASE_URL + '/' + profile_name, headers = profile_headers)
+	profile_resp = requests.get(BASE_URL + f"/{profile_name}/" , headers = profile_headers)
 	csrftoken = profile_resp.cookies.get('csrftoken')
 	if profile_resp.status_code == 200:
 		resp = profile_resp.content
@@ -100,52 +102,52 @@ def Scap(ssid):
 				print('---- --Some Error like Session Timeout... ! try to login again?-- -----')
 				login()
 			else:
-				profile_json = parse__json(True ,extract__profile__json(True , resp))
+				profile_json = parse__json(True , extract__profile__json(True , resp))
 				USER_DICT = profile_json['user']
 				global PROFILE_NAME_DIR 
 				PROFILE_NAME_DIR = USER_DICT['full_name']
 				print('>>>>>>> @' + USER_DICT['full_name'])
 				print('>>>>>>> ' + USER_DICT['biography'])
-				print('>>>>>>> follower: '+str(USER_DICT['edge_followed_by']['count']))
-				print('>>>>>>> all media: ' +str(USER_DICT['edge_owner_to_timeline_media']['count']))
+				print('>>>>>>> follower: ' + str(USER_DICT['edge_followed_by']['count']))
+				print('>>>>>>> all media: ' + str(USER_DICT['edge_owner_to_timeline_media']['count']))
 				id = USER_DICT["id"]
 				# I has finding and try using url for scap private profile but it didn't work for me ! I don't know why ^_^ '
 				if len(USER_DICT['edge_owner_to_timeline_media']['edges'] ) <= 0:
 				 	if USER_DICT['is_private']:
 				 		print('---------- this profile is Private ! ------------')
 				 		profile_headers.update({'x-csrftoken':csrftoken})
-				 		follow_resp = requests.post(FOLLOW_ENDPOINT.format(str(id)), headers = profile_headers, allow_redirects = False).status_code
+				 		follow_resp = requests.post(FOLLOW_ENDPOINT.format(str(id)) , headers = profile_headers , allow_redirects = False).status_code
 				 		if follow_resp == 302:
 				 			print('√ Follow request has sending... go back here when this profile accept the follow request ! LOL =))) ')
 				 		
 				 	else:
 				 		print('------- Cannot find any media in this Profile ^_^ -------')
 				else:
-					print('Counting all page of profile @'+USER_DICT['full_name']+'....')
-					Real__Scrap(USER_DICT, id, ssid, profile_headers)
+					print(f"Counting all page of profile @{USER_DICT['full_name']}....")
+					Real__Scrap(USER_DICT , id , ssid , profile_headers)
 					
 	elif profile_resp.status_code == 404:
 		print('------------- 404 PAGE NOT FOUND ! -----------')
 	else:
 		print('------------------ ERROR ! ---------------')
 		
-def Real__Scrap(dict,id , ssid, header):
+def Real__Scrap(dict , id , ssid , header):
 	count = 0
 	NEXT_DICT = []
 	while True:
-		print('[Counting]> profile @'+dict['full_name'] + ' >Page: ' + str(count+1))
+		print('[Counting]> profile @'+dict['full_name'] + ' >Page: ' + str(count + 1))
 		
 		if count <= 0:
 			cursor = ""
 		else:
 			cursor = NEXT_DICT[count-1]['user']['edge_owner_to_timeline_media']['page_info']['end_cursor']
 		next_query = {'variables':'{'+'"id":"{}","first":{},"after":"{}"'.format(id,'12',cursor)+'}'}
-		next_resp = requests.get(BASE_URL+QUERY_ENDPOINT,params=next_query, headers = header).content
-		next_json = parse__json(False, extract__profile__json(False,next_resp))
+		next_resp = requests.get(BASE_URL + QUERY_ENDPOINT ,params = next_query , headers = header).content
+		next_json = parse__json(False, extract__profile__json(False , next_resp))
 		NEXT_DICT.append(next_json)
 		if not next_json['user']['edge_owner_to_timeline_media']['page_info']['has_next_page']:
 			break
-		count +=1
+		count += 1
 	extract__dict(NEXT_DICT)
 	
 		
@@ -154,7 +156,7 @@ def extract__profile__json(isFirst , str):
 	jsons = "{ }"
 	if isFirst:
 		regex = rb".*window._sharedData\s*=\s*(.+?});"
-		match = re.search(regex, str)
+		match = re.search(regex , str)
 		jsons = json.loads(match.group(1))
 	else:
 		jsons = json.loads(str)
@@ -169,22 +171,22 @@ def parse__json(isFirst , json):
 			j = json["data"]
 		return j
 	except:
-		print('--- ERROR ---')
+		print('--- ERROR : invalid JSON or response is not JSON ---')
 	    
 def un_login(resp):
-	    regex = r'.*Login\s*•\s+Instagram'
-	    isMatch = re.search(regex , resp.decode('utf-8'))
-	    if isMatch:
-	    	return True
-	    else:
-	    	return False
+    regex = rb"<title>\n.*Login"
+	isMatch = re.search(regex , resp , re.MULTILINE)
+	if isMatch:
+	    return True
+	else:
+    	return False
 	    
 def extract__dict(dict):
-	    count = 0
-	    for item in dict:
-    		for edges in item['user']['edge_owner_to_timeline_media']['edges']:
-	    		extract__(edges['node'], str(count + 1))
-	    	count +=1
+    count = 0
+	for item in dict:
+    	for edges in item['user']['edge_owner_to_timeline_media']['edges']:
+	    	extract__(edges['node'], str(count + 1))
+	    count += 1
 	    
 def extract__image(node):
     return node['display_url']
@@ -203,21 +205,21 @@ def extract__(node, page):
         for item in node_child:
             node_child_of_child = item['node']
             child_type = node_child_of_child['__typename']
-            download(extract__image(node_child_of_child), page) if child_type == TYPEIMAGE else download(extract__video(node_child_of_child), page)
+            download(extract__image(node_child_of_child) , page) if child_type == TYPEIMAGE else download(extract__video(node_child_of_child) , page)
             
 
 def download(url, page):
     buffer_size = 1024
-    response = requests.get(url, stream=True)
-    file_size = int(response.headers.get("Content-Length", 0))
+    response = requests.get(url , stream=True)
+    file_size = int(response.headers.get("Content-Length" , 0))
     default_filename = get_filename(url)
     content_disposition = response.headers.get("Content-Disposition")
     if content_disposition:
-        value, params = cgi.parse_header(content_disposition)
-        filename = params.get("filename", default_filename)
+        value , params = cgi.parse_header(content_disposition)
+        filename = params.get("filename" , default_filename)
     else:
         filename = default_filename
-    progress = tqdm(response.iter_content(buffer_size), f"Page: {page} > Saved {filename}...", total=file_size, unit="B", unit_scale=True, unit_divisor=1024)
+    progress = tqdm(response.iter_content(buffer_size) , f"Page: {page} > Saved {filename}...." , total=file_size , unit="B" , unit_scale=True , unit_divisor=1024)
     media_path = MEDIA_DL_DIR + '/' + PROFILE_NAME_DIR 
     full_path = media_path + '/' + "Page " + str(page)
     if not os.path.exists(MEDIA_DL_DIR):
@@ -243,3 +245,6 @@ def get_filename(url):
        	    
 if __name__ == '__main__':
     main()
+
+
+#    ^_^     #
